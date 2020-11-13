7775534a18f2ffbcc74c8113740b3bdaae450419
Gwerborc'h Mahéo <gwerborch.maheo.com>
2020-11-13 15:32:31 +0100

    done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,31 @@
-// Iteration 1: Names and Input
+//Iteration 1: Names and Input
 
+let hacker1;
+hacker1 = "Gwerborc’h";
+console.log(hacker1);
+let hacker2;
+hacker2 = "Omar";
+console.log(hacker2);
+//Iteration 2: Conditionals
 
-// Iteration 2: Conditionals
+if(hacker1.length > hacker2.length) {
+  console.log(`The longest name ${hacker1}.`);
+} else if(hacker1.length < hacker2.length) {
+  console.log(`The longest name ${hacker2}.`);
+} else {
+  console.log("The both names are equally long");
+}
+//Iteration 3: Loops
 
-
-// Iteration 3: Loops
+function letters(chaine) {
+  chaine = chaine.toUpperCase();
+  let chaine1;
+  chaine1 = chaine.split('');
+  console.log(chaine1.join('     '));
+ }
+letters(hacker1);
+letters(hacker2);
+hacker1 = hacker1.split('').reverse().join('');
+console.log(hacker1);
+hacker2 = hacker2.split('').reverse().join('');
+console.log(hacker2);
